9fadd53fa228cdfd8dcc9fc37b380abc41d443b7
jreidy <jreidy@stanford.edu>
2013-12-22 06:26:22 -0700

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WoodRiverScholars::Application.config.secret_key_base = 'a16068c8b0f8b236820fe6255bf21d57736664dfbf7afa3acd3f7fe0dfbf477c21722a55bbcb5ab58a68a4c09c3478c334e13c13e133547a4edf641fb0d4b6f8'
